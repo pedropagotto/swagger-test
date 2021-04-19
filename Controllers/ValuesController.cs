ecc0442c95cd347bcd94f160895fd81fca39ef1a
Pedro Pagotto <pedropagotto@outlook.com>
2021-04-19 20:45:51 -0300

    first commit test

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi_swagger.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/<ValuesController>
+        [HttpPost]
+        [ProducesResponseType(typeof(UnauthorizedResult), 401)]
+        [ProducesResponseType(typeof(ForbidResult), 403)]
+        [ProducesResponseType(typeof(List<ListParam>), 200)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
+        [ProducesResponseType(typeof(ProblemDetails), 404)]
+        [ProducesResponseType(typeof(ProblemDetails), 409)]
+        public ActionResult<List<ListParam>> Post([FromBody] ListParam request)
+        {
+            var response = new List<ListParam>();
+            response.Add(request);
+            return response;
+        }
+    }
+}
---
+++ ListParam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace webapi_swagger
+{
+    public class ListParam
+    {
+        [Required]
+        public int recordStart { get; set; }
+        [Required]
+        public int recordEnd { get; set; }
+
+        public FilterApi? filter { get; set; }
+    }
+
+    public class FilterApi
+    {
+        public bool IsExempted { get; set; }
+        public List<string> CustomerCode { get; set; } = new List<string>();
+    }
+}
